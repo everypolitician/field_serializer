202d7b26fefaa9699556481a772c2732d8721376
Mark Longair <mhl@pobox.com>
2016-09-28 16:51:08 +0100

    Implementation of fields (and returning in to_h)

--- lib/field_serializer.rb
+++ lib/field_serializer.rb
@@ -1,7 +1,26 @@
 require "field_serializer/version"
 
 module FieldSerializer
+  def self.included(klass)
+    klass.extend(ClassMethods)
+  end
+
+  module ClassMethods
+
+    def fields
+      @fields ||= {}
+    end
+
+    def field(name, &block)
+      fields[name] = block
+    end
+
+  end
+
   def to_h
-    {}
+    self.class.fields.map { |name, block|
+      v = instance_eval(&block) rescue nil
+      [name, v]
+    }.to_h
   end
 end
