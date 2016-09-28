09acb67d04b6dbad44da06f9500ae76379f2bc85
Oliver Denman <oliver@therewillbeanimals.com>
2016-09-28 16:32:36 +0100

    Add test for version number

--- test/field_serializer_test.rb
+++ test/field_serializer_test.rb
@@ -1,11 +1,7 @@
 require 'test_helper'
 
-class FieldSerializerTest < Minitest::Test
-  def test_that_it_has_a_version_number
-    refute_nil ::FieldSerializer::VERSION
-  end
-
-  def test_it_does_something_useful
-    assert false
+describe FieldSerializer  do
+  it 'should have a version number' do
+    FieldSerializer::VERSION.wont_be_nil
   end
 end
