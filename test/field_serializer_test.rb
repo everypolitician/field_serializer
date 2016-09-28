e2b1dab98509aab27a14a2b8e8d5b70d86769ea2
Chris Mytton <chrismytton@gmail.com>
2016-09-28 16:42:01 +0100

    Add failing test for single field class

--- test/field_serializer_test.rb
+++ test/field_serializer_test.rb
@@ -5,13 +5,31 @@ describe FieldSerializer  do
     FieldSerializer::VERSION.wont_be_nil
   end
 
-  class TestThing
-    include FieldSerializer
+  describe 'class with no fields' do
+    class EmptyTestThing
+      include FieldSerializer
+    end
+
+    describe '#to_h' do
+      it 'should return an empty hash' do
+        EmptyTestThing.new.to_h.must_equal({})
+      end
+    end
   end
 
-  describe '#to_h' do
-    it 'should return an empty hash' do
-      TestThing.new.to_h.must_equal({})
+  describe 'class with one field' do
+    class OneFieldTestThing
+      include FieldSerializer
+
+      field :foo do
+        :bar
+      end
+    end
+
+    describe '#to_h' do
+      it 'should return an empty hash' do
+        OneFieldTestThing.new.to_h.must_equal foo: :bar
+      end
     end
   end
 end
