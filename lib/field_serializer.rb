09310e4330ec6be465f85df1abb4ccd0043d2e03
Oliver Denman <oliver@therewillbeanimals.com>
2016-10-17 15:50:59 +0100

    Include parent class fields in to_h result
    Previously, if a class inherited from a parent class with fields
    defined in it, the subclass would not return the parent class fields in
    its #to_h result:
    
    InheritanceTest#methods: constituency
    InheritanceSubclassTest#methods: state
    
    > to_h
    => {:state=>"Zamfara"}
    
    This commit fixes the bug.
    
    > to_h
    => {:constituency=>"Bungudu", :state=>"Zamfara"}

--- lib/field_serializer.rb
+++ lib/field_serializer.rb
@@ -23,9 +23,30 @@ module FieldSerializer
   end
 
   def to_h
-    self.class.fields.map { |name|
+    all_fields.map { |name|
       v = __send__(name)
       [name, v]
     }.to_h
+  end
+
+  private
+
+  def all_fields
+    super_fields + self.class.fields
+  end
+
+  def super_fields
+    return [] unless superclass_implements_field_serializer?
+    self.class.superclass.fields
+  end
+
+  def superclass_implements_field_serializer?
+    superclass_responds_to?(:field, :fields)
+  end
+
+  def superclass_responds_to?(*methods)
+    methods.reduce('true') do | r, method |
+      r &&= self.class.superclass.respond_to? method
+    end
   end
 end
