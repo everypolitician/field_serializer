eafe930d6cc7f281c1e8ffc15e967bccebdfb072
Chris Mytton <chrismytton@gmail.com>
2016-11-04 11:24:31 +0100

    Add FieldSerializer::ClassMethods#inherited hook
    This allows us to run some code when a subclass is created, in this case
    we load up the subclass with the `fields` attribute from the parent
    class.
    
    I was guided to this solution by this answer on StackOverflow:
    http://stackoverflow.com/a/13629724

--- lib/field_serializer.rb
+++ lib/field_serializer.rb
@@ -10,6 +10,9 @@ module FieldSerializer
   end
 
   module ClassMethods
+    def inherited(subclass)
+      subclass.fields.concat(fields)
+    end
 
     def fields
       @fields ||= []
@@ -23,30 +26,9 @@ module FieldSerializer
   end
 
   def to_h
-    all_fields.map { |name|
+    self.class.fields.map { |name|
       v = __send__(name)
       [name, v]
     }.to_h
-  end
-
-  private
-
-  def all_fields
-    super_fields + self.class.fields
-  end
-
-  def super_fields
-    return [] unless superclass_implements_field_serializer?
-    self.class.superclass.fields
-  end
-
-  def superclass_implements_field_serializer?
-    superclass_responds_to?(:field, :fields)
-  end
-
-  def superclass_responds_to?(*methods)
-    methods.reduce('true') do | r, method |
-      r &&= self.class.superclass.respond_to? method
-    end
   end
 end
